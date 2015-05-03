dd4bf1712014ba9ab1f5a4e22c8c0a42d7cdf5f8
Heine Furubotten <hfurubotten@gmail.com>
2015-05-04 00:09:52 +0200

    Easier search for java files in test script

--- lab/test.sh
+++ lab/test.sh
@@ -2,5 +2,7 @@
 
 cd ../
 pwd
-javac -cp /usr/share/java/junit4-4.11.jar /testground/labs/src/test/java/no/uis/intro/*.java /testground/labs/src/main/java/no/uis/intro/*.java /testground/labs/src/main/java/autograder/Score.java
+
+find -name "*.java" > source.txt
+javac -cp /usr/share/java/junit4-4.11.jar @source.txt
 java -cp /usr/share/java/junit4-4.11.jar:/usr/share/java/hamcrest-core-1.3.jar:/testground/labs/src/ org.junit.runner.JUnitCore test.java.no.uis.intro.FibonacciSequenceTest test.java.no.uis.intro.ArithmeticSequenceTest test.java.no.uis.intro.GeometricSequenceTest 
