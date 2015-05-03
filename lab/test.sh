7913aea30c7a615d34a91fc669fdde9abfd3f5be
Heine Furubotten <hfurubotten@gmail.com>
2015-05-03 20:24:50 +0200

    sh script directs to correct folder and have more exact file paths.

--- lab/test.sh
+++ lab/test.sh
@@ -1,4 +1,5 @@
 #/bin/sh
 
-javac -cp /usr/share/java/junit4-4.11.jar src/test/java/no/uis/intro/*.java src/main/java/no/uis/intro/*.java
-java -cp /usr/share/java/junit4-4.11.jar:/usr/share/java/hamcrest-core-1.3.jar:/testground/uis-java-intro/labs-test/src/ org.junit.runner.JUnitCore test.java.no.uis.intro.FibonacciSequenceTest test.java.no.uis.intro.ArithmeticSequenceTest test.java.no.uis.intro.GeometricSequenceTest 
+cd ../
+javac -cp /usr/share/java/junit4-4.11.jar /testground/uis-java-intro/labs/src/test/java/no/uis/intro/*.java /testground/uis-java-intro/labs/src/main/java/no/uis/intro/*.java /testground/uis-java-intro/labs/src/main/java/autograder/Score.java
+java -cp /usr/share/java/junit4-4.11.jar:/usr/share/java/hamcrest-core-1.3.jar:/testground/uis-java-intro/labs/src/ org.junit.runner.JUnitCore test.java.no.uis.intro.FibonacciSequenceTest test.java.no.uis.intro.ArithmeticSequenceTest test.java.no.uis.intro.GeometricSequenceTest 
