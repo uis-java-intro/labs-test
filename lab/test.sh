25867bbb6fa38cad16ab71bc61a03a25ebe7f9d6
Heine Furubotten <hfurubotten@gmail.com>
2015-05-03 20:28:59 +0200

    corrected class path in sh script

--- lab/test.sh
+++ lab/test.sh
@@ -1,5 +1,6 @@
 #/bin/sh
 
 cd ../
-javac -cp /usr/share/java/junit4-4.11.jar /testground/uis-java-intro/labs/src/test/java/no/uis/intro/*.java /testground/uis-java-intro/labs/src/main/java/no/uis/intro/*.java /testground/uis-java-intro/labs/src/main/java/autograder/Score.java
-java -cp /usr/share/java/junit4-4.11.jar:/usr/share/java/hamcrest-core-1.3.jar:/testground/uis-java-intro/labs/src/ org.junit.runner.JUnitCore test.java.no.uis.intro.FibonacciSequenceTest test.java.no.uis.intro.ArithmeticSequenceTest test.java.no.uis.intro.GeometricSequenceTest 
+pwd
+javac -cp /usr/share/java/junit4-4.11.jar /testground/labs/src/test/java/no/uis/intro/*.java /testground/labs/src/main/java/no/uis/intro/*.java /testground/labs/src/main/java/autograder/Score.java
+java -cp /usr/share/java/junit4-4.11.jar:/usr/share/java/hamcrest-core-1.3.jar:/testground/labs/src/ org.junit.runner.JUnitCore test.java.no.uis.intro.FibonacciSequenceTest test.java.no.uis.intro.ArithmeticSequenceTest test.java.no.uis.intro.GeometricSequenceTest 
